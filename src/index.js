2042a96415653f427cd8d07dae829c9b74a3a46a
Aaron Harvey <Harvey.AaronJ@gmail.com>
2022-03-14 14:37:09 -0400

    Changes excercise to overlapping elements

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<head>
-</head>
-<body>
-  <header>
-    <h1>Welcome!</h1>
-  </header>
-  <section id="left">
-    <h2>Content</h2>
-    <p>Good stuff</p>
-  </section>
-  <aside id="right">
-    <h2>Sidebar</h2>
-    <p>Links</p>
-  </aside>
-</body>
+<div class="first"></div>
+<div class="second"></div>
 `;
