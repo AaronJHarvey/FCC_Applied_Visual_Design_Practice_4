e5bfb4de410bc2c72e9f9251f345f173304cb18c
Aaron Harvey <Harvey.AaronJ@gmail.com>
2022-03-14 16:57:33 -0400

    Sets Initial Exercise - Create a Gradual CSS Linear Gradient

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<header>
-  <h1>Cooking with FCC!</h1>
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Classes</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
+<div></div>
 </header>
 `;
