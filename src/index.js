6683995cf4872d4e3b78d7ec1e79d969900f9d9e
Aaron Harvey <Harvey.AaronJ@gmail.com>
2022-03-14 12:14:18 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<body>
+  <h1>Welcome!</h1>
+  <section>
+    <form id="searchbar">
+      <label for="search">Search:</label>
+      <input type="search" id="search" name="search">
+      <input type="submit" name="submit" value="Go!">
+    </form>
+  </section>
+</body>
+`;
